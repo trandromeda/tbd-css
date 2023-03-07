c5da1411db9767f26bc0352c7ef7433f785ecdb4
Andy Tran <transient.tran@gmail.com>
2023-03-06 21:43:57 -0500

    feat: install and use react

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { render } from "react-dom";
+ 
+const App = () => {
+  return (
+    <div>
+      <p>Pagepro best react team in the world</p>
+    </div>
+  );
+};
+ 
+render(<App />, document.getElementById("app"));
